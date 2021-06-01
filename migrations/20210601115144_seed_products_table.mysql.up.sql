1ed167e9949a9ebee781566193420bd105cd1843
Alvin <git-commit-address>
2021-06-02 02:29:57 +0800

    added some mock products

--- migrations/20210601115144_seed_products_table.mysql.up.sql
+++ migrations/20210601115144_seed_products_table.mysql.up.sql
@@ -17,9 +17,89 @@ INSERT INTO `gorent`.`products` (
 ),(
     '1', 
     'Yonex', 
-    'Yonex badminton racket', 
+    'yonex badminton racket', 
     '0', 
     'Legendary awesome racket',
+    '2021-06-01',
+    '2021-06-01'
+),(
+    '1', 
+    'Seba', 
+    'inline roller skates', 
+    '0', 
+    'Renting out my beloved pair of skates as I no longer use them because of an new knee injury but dont wish to sell them because it carries sentimental values.',
+    '2021-06-01',
+    '2021-06-01'
+),(
+    '1', 
+    'For Dummies', 
+    'go for dummies', 
+    '0', 
+    'If youve been struggling with picking up GO as a programming language, this book will definitely change the way you think of go. Contains 101 secrets used by top professionals in the industry.',
+    '2021-06-01',
+    '2021-06-01'
+),(
+    '1', 
+    'Crumpler', 
+    'camera bag', 
+    '0', 
+    'A good photographer invest in their camera bodies and lensese. An Excellent one invests in protection for their gear. The crumpler 6 divisoin camera bag is bound to offer the best protection even for your top range gears. Comes with extra padding if you need them!',
+    '2021-06-01',
+    '2021-06-01'
+),(
+    '1', 
+    'Casio', 
+    'graphical calculator', 
+    '0', 
+    'Legendary awesome racket',
+    '2021-06-01',
+    '2021-06-01'
+),(
+    '1', 
+    'Dell', 
+    'laptop bag', 
+    '0', 
+    'Got it free from the IT department when I reported at work for the first day. It offeres a lot of protection for your laptop but it is too chunky for me. Renting off cheap.',
+    '2021-06-01',
+    '2021-06-01'
+),(
+    '1', 
+    'HP', 
+    'monochrome printer', 
+    '0', 
+    'This printer is not just your run-of-the-mill printer. It is a monochrome printer. That means it doesnt just print in black and white. It also doesnt scream at you when you run out of red, green even when you do not use those colors',
+    '2021-06-01',
+    '2021-06-01'
+),(
+    '1', 
+    'Dell', 
+    'ultra wide monitor', 
+    '0', 
+    'Have to work in excel and have to scroll countless of times to the left or right? Well, youre not alone. This monitor right here will be your best buddy when you need it for your work or assignment. With the horizontal length more than 2.5 times a regular 11 inch macbook pro, this monitor is bound to help you improve your efficiency!',
+    '2021-06-01',
+    '2021-06-01'
+),(
+    '1', 
+    'HP', 
+    'school computer laptops', 
+    '0', 
+    'All these computer laptops are availale for rent. They are sanizited frequently and definitely after they are returned to us. All 12 of them are good for you to host your own programming classes. If you need more quantity, please feel free to ask or PM.',
+    '2021-06-01',
+    '2021-06-01'
+),(
+    '1', 
+    'PrettyBride', 
+    'pretty wedding dress', 
+    '0', 
+    'I bought this dress during my wedding and feel that its such a waste if I just let it stay in the closet. It comes with these fancy shiny beads that help make you sparkle even more while you walk down the aisle! While I cannot fit into this wedding dress anymore, Ill be glad to see someone else in it',
+    '2021-06-01',
+    '2021-06-01'
+),(
+    '1', 
+    'Disney', 
+    'lion king movie cd', 
+    '0', 
+    'You know, one day our children wont even know what a CD player is. Rent this, take this time to educate them on what a CD players is. Besides, lion king is very good show about children. #ensureCDplayerGetsPassedDown',
     '2021-06-01',
     '2021-06-01'
 );
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 go run cmd/web/*.go \
--dbuser='' \
--dbpassword='' \
--dbname='' \
--dbhost='' \
--dbport=''  \
--port=''
+-dbuser='root' \
+-dbpassword='S8830571z$' \
+-dbname='goRent' \
+-dbhost='localhost' \
+-dbport='3306'  \
+-port='8080'
