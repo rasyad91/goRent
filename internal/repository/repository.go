363b5a6b246c1f2224d00843f57c62a1edb5b7fa
Rasyad <rasyadsubandrio@gmail.com>
2021-06-02 14:57:24 +0800

    merge Search

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -4,9 +4,6 @@ import "goRent/internal/model"
 
 type DatabaseRepo interface {
 	GetAllCourses()
-<<<<<<< HEAD
 	GetUser(username string) (model.User, bool)
-=======
 	GetAllProducts() ([]model.Product, error)
->>>>>>> origin/alvinProducts
 }
