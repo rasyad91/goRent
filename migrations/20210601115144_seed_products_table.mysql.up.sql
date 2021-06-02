5baecd884aab6f1d203a84ea49e9cb6936699c5a
Alvin <git-commit-address>
2021-06-02 10:49:51 +0800

    udpated products table with price column added

--- migrations/20210601115144_seed_products_table.mysql.up.sql
+++ migrations/20210601115144_seed_products_table.mysql.up.sql
@@ -4,6 +4,7 @@ INSERT INTO `gorent`.`products` (
     `title`, 
     `rating`, 
     `description`, 
+    `price`,
     `created_at`,
     `updated_at`
 ) VALUES (
@@ -11,7 +12,8 @@ INSERT INTO `gorent`.`products` (
     'Nike', 
     'Nike Vaporfly', 
     '0', 
-    'Nike shoe',
+    'Super Limited Edition Nike shoe. Weaar once and these shoes will make you take off. Experience the all new air jordans today!',
+    '49.99',
     '2021-06-01',
     '2021-06-01'
 ),(
@@ -20,6 +22,7 @@ INSERT INTO `gorent`.`products` (
     'yonex badminton racket', 
     '0', 
     'Legendary awesome racket',
+    '7.49',  
     '2021-06-01',
     '2021-06-01'
 ),(
@@ -28,6 +31,7 @@ INSERT INTO `gorent`.`products` (
     'inline roller skates', 
     '0', 
     'Renting out my beloved pair of skates as I no longer use them because of an new knee injury but dont wish to sell them because it carries sentimental values.',
+    '9.99',
     '2021-06-01',
     '2021-06-01'
 ),(
@@ -36,6 +40,7 @@ INSERT INTO `gorent`.`products` (
     'go for dummies', 
     '0', 
     'If youve been struggling with picking up GO as a programming language, this book will definitely change the way you think of go. Contains 101 secrets used by top professionals in the industry.',
+    '2.94',
     '2021-06-01',
     '2021-06-01'
 ),(
@@ -44,6 +49,7 @@ INSERT INTO `gorent`.`products` (
     'camera bag', 
     '0', 
     'A good photographer invest in their camera bodies and lensese. An Excellent one invests in protection for their gear. The crumpler 6 divisoin camera bag is bound to offer the best protection even for your top range gears. Comes with extra padding if you need them!',
+    '5.99',
     '2021-06-01',
     '2021-06-01'
 ),(
@@ -52,6 +58,7 @@ INSERT INTO `gorent`.`products` (
     'graphical calculator', 
     '0', 
     'Legendary awesome racket',
+    '1.00',
     '2021-06-01',
     '2021-06-01'
 ),(
@@ -60,6 +67,7 @@ INSERT INTO `gorent`.`products` (
     'laptop bag', 
     '0', 
     'Got it free from the IT department when I reported at work for the first day. It offeres a lot of protection for your laptop but it is too chunky for me. Renting off cheap.',
+    '0.50',
     '2021-06-01',
     '2021-06-01'
 ),(
@@ -68,6 +76,7 @@ INSERT INTO `gorent`.`products` (
     'monochrome printer', 
     '0', 
     'This printer is not just your run-of-the-mill printer. It is a monochrome printer. That means it doesnt just print in black and white. It also doesnt scream at you when you run out of red, green even when you do not use those colors',
+    '9.99',
     '2021-06-01',
     '2021-06-01'
 ),(
@@ -76,6 +85,7 @@ INSERT INTO `gorent`.`products` (
     'ultra wide monitor', 
     '0', 
     'Have to work in excel and have to scroll countless of times to the left or right? Well, youre not alone. This monitor right here will be your best buddy when you need it for your work or assignment. With the horizontal length more than 2.5 times a regular 11 inch macbook pro, this monitor is bound to help you improve your efficiency!',
+    '4.99',
     '2021-06-01',
     '2021-06-01'
 ),(
@@ -84,6 +94,7 @@ INSERT INTO `gorent`.`products` (
     'school computer laptops', 
     '0', 
     'All these computer laptops are availale for rent. They are sanizited frequently and definitely after they are returned to us. All 12 of them are good for you to host your own programming classes. If you need more quantity, please feel free to ask or PM.',
+    '129.99',
     '2021-06-01',
     '2021-06-01'
 ),(
@@ -92,6 +103,7 @@ INSERT INTO `gorent`.`products` (
     'pretty wedding dress', 
     '0', 
     'I bought this dress during my wedding and feel that its such a waste if I just let it stay in the closet. It comes with these fancy shiny beads that help make you sparkle even more while you walk down the aisle! While I cannot fit into this wedding dress anymore, Ill be glad to see someone else in it',
+    '88.88',
     '2021-06-01',
     '2021-06-01'
 ),(
@@ -100,6 +112,7 @@ INSERT INTO `gorent`.`products` (
     'lion king movie cd', 
     '0', 
     'You know, one day our children wont even know what a CD player is. Rent this, take this time to educate them on what a CD players is. Besides, lion king is very good show about children. #ensureCDplayerGetsPassedDown',
+    '3.99',
     '2021-06-01',
     '2021-06-01'
 );
