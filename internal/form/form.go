d19379b4d160fca4259367cbacb91347410c9b94
Rasyad <rasyadsubandrio@gmail.com>
2021-06-03 06:15:07 +0800

    add checklength function in form

--- internal/form/form.go
+++ internal/form/form.go
@@ -32,6 +32,21 @@ func (e errors) Get(field string) string {
 	return e[field][0]
 }
 
+// CheckLength validates the length of the input string, if there is no max length, enter negative
+// integer (eg. -1) for max field
+func (f *Form) CheckLength(field string, min, max int) {
+	value := f.Get(field)
+	if max < 0 {
+		if len(value) < min {
+			f.Errors.Add(field, fmt.Sprintf("%s min. length is %d", field, min))
+		}
+	} else {
+		if len(value) < min || len(value) > max {
+			f.Errors.Add(field, fmt.Sprintf("%s min.length: %d and max.length: %d", field, min, max))
+		}
+	}
+}
+
 func (f *Form) Required(fields ...string) {
 	for _, field := range fields {
 		value := f.Get(field)
