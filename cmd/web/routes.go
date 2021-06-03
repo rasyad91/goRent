294ac21a01c761e6814102a8e87d406ff85c98c2
Rasyad <rasyadsubandrio@gmail.com>
2021-06-03 18:42:13 +0800

    resolved merge issues

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -16,19 +16,13 @@ func routes() http.Handler {
 	mux.Use(RecoverPanic)
 	mux.Use(NoSurf)
 
-<<<<<<< HEAD
 	mux.HandleFunc("/", handler.Repo.Home).Methods("GET")
 	mux.HandleFunc("/search", handler.Repo.Search).Methods("GET")
 	mux.HandleFunc("/searchresult", handler.Repo.SearchResult).Methods("GET")
-=======
-<<<<<<< HEAD
-=======
-	router.HandleFunc("/user/logout", handler.Repo.Logout).Methods("GET")
+	mux.HandleFunc("/user/logout", handler.Repo.Logout).Methods("GET")
 
->>>>>>> Login
-	router.HandleFunc("/login", handler.Repo.Login).Methods("GET")
-	router.HandleFunc("/login", handler.Repo.LoginPost).Methods("POST")
->>>>>>> d0bb68ff913320fbff4313a78419e5157181b05e
+	mux.HandleFunc("/login", handler.Repo.Login).Methods("GET")
+	mux.HandleFunc("/login", handler.Repo.LoginPost).Methods("POST")
 
 	mux.HandleFunc("/register", handler.Repo.Register).Methods("GET")
 	mux.HandleFunc("/register", handler.Repo.RegisterPost).Methods("POST")
@@ -46,10 +40,10 @@ func routes() http.Handler {
 
 	// fileServer := http.FileServer(http.Dir("./static/"))
 	// mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-		// static files
-		fileServer := http.FileServer(http.Dir("./static/"))
-		mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
-		// mux.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
+	// static files
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
+	// mux.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
 
 	return mux
 }
