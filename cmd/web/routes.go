3d3f6230a4f1e55956eb36f567c932b67ce101a7
Rasyad <rasyadsubandrio@gmail.com>
2021-06-03 13:29:39 +0800

    update user route

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -16,16 +16,19 @@ func routes() http.Handler {
 	router.Use(RecoverPanic)
 	router.Use(NoSurf)
 
+	router.HandleFunc("/", handler.Repo.Home).Methods("GET")
 	router.HandleFunc("/search", handler.Repo.Search).Methods("GET")
 	router.HandleFunc("/searchresult", handler.Repo.SearchResult).Methods("GET")
-
-	router.HandleFunc("/", handler.Repo.Home).Methods("GET")
 
 	router.HandleFunc("/login", handler.Repo.Login).Methods("GET")
 	router.HandleFunc("/login", handler.Repo.Login).Methods("POST")
 
 	router.HandleFunc("/register", handler.Repo.Register).Methods("GET")
 	router.HandleFunc("/register", handler.Repo.RegisterPost).Methods("POST")
+
+	router.HandleFunc("/user/{userID}/bookings", handler.Repo.UserBookings).Methods("GET")
+	router.HandleFunc("/user/{userID}/rents", handler.Repo.UserRents).Methods("GET")
+	router.HandleFunc("/user/{userID}/products", handler.Repo.UserProducts).Methods("GET")
 
 	router.HandleFunc("/user/logout", handler.Repo.Logout).Methods("GET")
 
---
+++ internal/handler/users.go
@@ -0,0 +1,15 @@
+package handler
+
+import "net/http"
+
+func (m *Repository) UserBookings(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (m *Repository) UserProducts(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (m *Repository) UserRents(w http.ResponseWriter, r *http.Request) {
+
+}
