f075dc39a4a7597246645dcc5f6fa5acb898100b
Rachel  Tan <racheltan@RTs-Macbook-Pro.local>
2021-06-07 17:25:35 +0800

    changing form format

--- internal/handler/users.go
+++ internal/handler/users.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"fmt"
 	"goRent/internal/form"
 	"goRent/internal/model"
 	"goRent/internal/render"
@@ -10,32 +11,20 @@ import (
 func (m *Repository) UserAccount(w http.ResponseWriter, r *http.Request) {
 	data := make(map[string]interface{})
 
-	data["editUser"] = model.User{
-		Username: "",
-		Email:    "",
-		Password: "",
-		Address:  model.Address{},
-	}
 	if err := render.Template(w, r, "account.page.html", &render.TemplateData{
 		Data: data,
-		Form: &form.Form{},
 	}); err != nil {
 		m.App.Error.Println(err)
 	}
 }
-
-func (m *Repository) GetCart(w http.ResponseWriter, r *http.Request) {
-
-	// data := make(map[string]interface{})
-}
 func (m *Repository) EditUserAccount(w http.ResponseWriter, r *http.Request) {
 	data := make(map[string]interface{})
-
+	u := m.App.Session.Get(r.Context(), "user").(model.User)
 	data["editUser"] = model.User{
-		Username: "",
-		Email:    "",
+		Username: u.Username,
+		Email:    u.Email,
 		Password: "",
-		Address:  model.Address{},
+		Address:  u.Address,
 	}
 	if err := render.Template(w, r, "profile.page.html", &render.TemplateData{
 		Data: data,
@@ -43,6 +32,43 @@ func (m *Repository) EditUserAccount(w http.ResponseWriter, r *http.Request) {
 	}); err != nil {
 		m.App.Error.Println(err)
 	}
+}
+
+func (m *Repository) EditUserAccountPost(w http.ResponseWriter, r *http.Request) {
+	m.App.Info.Println("Register: POST")
+	// u := m.App.Session.Get(r.Context(), "user").(model.User)
+	// data := make(map[string]interface{})
+
+	if err := r.ParseForm(); err != nil {
+		m.App.Error.Println(err)
+	}
+	newUser := model.User{
+		Username: r.FormValue("username"),
+		Email:    r.FormValue("email"),
+		Password: r.FormValue("password"),
+		Address: model.Address{
+			Block:      r.FormValue("block"),
+			StreetName: r.FormValue("streetName"),
+			UnitNumber: r.FormValue("unitNumber"),
+			PostalCode: r.FormValue("postalCode"),
+		},
+	}
+
+	form := form.New(r.PostForm)
+	fmt.Println("FORM:", form)
+	form.Required("block", "streetName", "unitNumber", "postalCode")
+	form.CheckLength("block", 1, 10)
+	form.CheckLength("streetName", 1, 255)
+	form.CheckLength("unitNumber", 1, 10)
+	form.CheckLength("postalCode", 1, 10)
+	fmt.Println(newUser)
+	http.Redirect(w, r, "/login", http.StatusSeeOther)
+
+}
+
+func (m *Repository) GetCart(w http.ResponseWriter, r *http.Request) {
+
+	// data := make(map[string]interface{})
 }
 
 // func (m *Repository) UserAccount(w http.ResponseWriter, r *http.Request) {
